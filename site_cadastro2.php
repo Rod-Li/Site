a06737e985a7231f316f81051bf302c7b0ff1107
Felipe-CCO <55193972+Felipe-CCO@users.noreply.github.com>
2019-11-22 11:27:16 -0300

    Update site_cadastro2.php

--- site_cadastro2.php
+++ site_cadastro2.php
@@ -1,13 +1,19 @@
 <?php
-    $nome = $_POST["nome"];
-    $email = $_POST["email"];
-    $senha = $_POST["senha"];
+    include 'site_cadastro.php';
 
-    function Imprimir ($nome, $email, $senha){
-        echo "Nome: $nome <br/>";
-        echo "Email: $email <br/>";
-        echo "Senha: $senha <br/>";
-    }
+    //inserir ($nome, $email, $senha);
+    //inserir ("Rodrigo", "rodrigo@rodrigo","rodrigo");
 
-    Imprimir ($nome, $email, $senha);
+     /*$projeto = listarTodos();
+    while($projeto = mysqli_fetch_array($projeto)){
+        echo "Id: {$projeto['id']} <br/>";
+        echo "Nome: {$projeto['nome']} <br/>";
+        echo "Email: {$projeto['email']} <br/>";
+        echo "Senha: {$projeto['senha']} <br/>";
+    }*/
+
+    //alterar(3,"Felipe Viturino","felipe@viturino","felipe");
+
+    //deletar(9);
+
 ?>
