b5e7e872e34ebc061fb7ae2d9a4c68d57b397e34
Rod-Li <55189133+Rod-Li@users.noreply.github.com>
2019-11-28 08:48:12 -0300

    Create testsite.php

---
+++ testsite.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Borderlands Criador</title>
+
+        <style>
+            body{
+                color: rgb(255, 255, 255);
+                background-color: black;
+                font-size: 18px;
+            }
+            img {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 40%;
+            }
+            button {
+                color: rgb(255, 255, 255);
+                background-color: rgb(34, 34, 34);
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 40%;
+                font-size: 16px;
+            }
+            input {
+                color: rgb(255, 255, 255);
+                background-color: rgb(34, 34, 34);
+            }
+            a {
+                color: rgb(255, 10, 10);
+            }
+        </style>
+    </head>
+    <body>
+        <hr/><p><b>Criação De Armas Da Franquia Borderlands</b><br/>Com abilidades, onde dropa e texto vermelho</p><br/>
+        <p>Esse É Um Site Não Official Da Franquia</p>
+        <p>Va Para O Site Official Do Jogo: <a href="https://borderlands.com"> Clique Aqui</a><br/></p>
+
+        <br/><p><b>Crie Armas Para O Universo De Borderlands</b><br/>E Que Sua Invenção Seja Uma Das Bilhões De Armas</p><hr/>
+
+
+            <form action="testsite2.php" method="POST">
+                <button>Ir Criar Novas Armas</button>
+            </form>
+
+        <hr/><p><b>O Site Não Esta Terminado</b><br/>Espere ate ser completado</p>
+
+        <hr/><img src="https://borderlands.com/images/_generated/1x/images/welcome/welcome-claptrap.png?c7cac4a0a0a99eaf932301396454dab7"/><hr/>
+    </body>
+</html>
